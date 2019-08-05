9ed52179c13be182699d72def191a401dda3c2aa
guoenleialdwx <guoenlei@aldwx.com>
2019-08-05 10:06:01 +0800

    新统计平台项目

---
+++ spark/src/main/scala/com/guo/App.scala
@@ -0,0 +1,14 @@
+package com.guo
+
+/**
+ * Hello world!
+ *
+ */
+/*object App extends Application {
+  println( "Hello World!" )
+}*/
+object App {
+  def main(args: Array[String]): Unit = {
+    println("ni hao")
+  }
+}
